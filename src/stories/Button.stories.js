30c0177561fa9028b7a824a52004aab5ca303f9c
Anita <anitapachecoh@gmail.com>
2020-10-31 00:47:29 -0300

    as

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from "../components/Button";
 
-const button = {
+export default {
   title: 'Button',
   component: Button,
 };
-
-export default button;
 
 export const Default = () => <Button onClick={action('clicked')}>Default Button</Button>;
 export const Primary = () => <Button primary onClick={action('clicked')}>Primary Button</Button>;
